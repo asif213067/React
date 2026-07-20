7e7105a61fe9e49410d07d7ac31277061ea715fa
Asif Hosen <asifhosen105010@gmail.com>
2026-07-20 17:42:07 +0600

    complete notes-app project

--- Module2/07.notes-app/src/App.jsx
+++ Module2/07.notes-app/src/App.jsx
@@ -1,120 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [title, setTitle] = useState('');
+  const [note, setNote] = useState('');
+
+  const [task, setTask] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const copyTask = [...task];
+    copyTask.push({ title, note });
+    setTask(copyTask);
+    // console.log(copyTask);
+
+    setTitle("");
+    setNote("");
+  }
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+    console.log(copyTask[idx]);
+
+    copyTask.splice(idx, 1);
+    setTask(copyTask); 
+  }
+
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="h-screen lg:flex bg-black text-white">
+        <form onSubmit={submitHandler} className="flex flex-col gap-2 lg:w-1/2 items-start p-10">
+          <h1 className='text-3xl font-bold'>Your Notes</h1>
 
-      <div className="ticks"></div>
+          {/* for note tittle  */}
+          <input
+            className="px-5 py-3 font-medium border-2 w-full outline-none rounded"
+            type="text"
+            placeholder="Enter Your Note Tittle"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          {/* for note details  */}
+          <textarea
+            className="px-5 py-3 font-medium border-2 w-full h-30 outline-none rounded"
+            type="text"
+            placeholder="Write Details Here"
+            value={note}
+            onChange={(e) => {
+              setNote(e.target.value);
+            }}
+          />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          <button
+            className="bg-white text-black active:scale-95  px-5 py-3 border-2 w-full outline-none rounded font-medium"
+          >
+            Add Note
+          </button>
+
+        </form>
+        <div className=" bg-gray-900 lg:w-1/2 p-10 lg:border-l-2">
+          <h1 className='text-3xl font-bold'>Your Notes</h1>
+
+          <div className='flex flex-wrap gap-5 items-start justify-start mt-6 h-[90%] overflow-auto'>
+            {task.map((elem, idx) => {
+
+              return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+                <div>
+                  <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                  <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.note}</p>
+                </div>
+                <button onClick={() => {
+                  deleteNote(idx);
+                }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+              </div>
+            })}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
