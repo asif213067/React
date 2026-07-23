320138583ec0e4d8e46d7edd2b15be6102d0861e
Asif Hosen <asifhosen105010@gmail.com>
2026-07-23 12:34:13 +0600

    down to up dataflow completed

---
+++ Module2/14.down-to-up-dataflow/src/App.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+import { useState } from "react"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+
+    const [theme, setTheme] = useState("light")
+
+  return (
+    <div>
+      <h1>The theme is {theme} </h1>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  )
+}
+
+export default App
